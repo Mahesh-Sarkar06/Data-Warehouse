5537f0fd543eb94a9f4eb8658e5b3d842b285739
Mahesh Chandra Sarkar <42028213+Mahesh-Sarkar06@users.noreply.github.com>
2025-03-20 14:10:02 +0530

    Updated tables schema
    Some new columns are added while transformation. So added that columns in schema.

--- scripts/silver/table-schema.sql
+++ scripts/silver/table-schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE silver.crm_cust_info (
   cust_key VARCHAR(50),
   cust_firstname VARCHAR(50),
   cust_lastname VARCHAR(50),
-  cust_marital_status VARCHAR(5),
-  cust_gndr VARCHAR(5),
+  cust_marital_status VARCHAR(10),
+  cust_gndr VARCHAR(10),
   cust_create_date DATE,
   dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -13,10 +13,12 @@ CREATE TABLE silver.crm_cust_info (
 -- Create table under SILVER LAYER for CRM/prod_info file
 CREATE TABLE silver.crm_prod_info (
   prod_id INT,
-  prod_key VARCHAR(50),
+  prod_key VARCHAR(25),
+  prod_cat_id VARCHAR(10),
+  prod_cat_key VARCHAR(15),
   prod_name VARCHAR(50),
   prod_cost FLOAT,
-  prod_line VARCHAR(5),
+  prod_line VARCHAR(25),
   prod_start_date DATE,
   prod_end_date DATE,
   dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -24,13 +26,13 @@ CREATE TABLE silver.crm_prod_info (
 
 -- Create table under SILVER LAYER for CRM/sales_details file
 CREATE TABLE silver.crm_sales_details (
-  sale_ord_num VARCHAR(50),
-  sale_prod_key VARCHAR(50),
+  sale_ord_num VARCHAR(25),
+  sale_prod_key VARCHAR(25),
   sale_cust_id INT,
-  sale_ord_date INT,
-  sale_ship_date INT,
-  sale_due_date INT,
-  sales INT,
+  sale_ord_date DATE,
+  sale_ship_date DATE,
+  sale_due_date DATE,
+  sales FLOAT,
   quantity INT,
   sale_price FLOAT,
   dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
